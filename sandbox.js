cf47a8ab1c7283537d506874a84648849b0330b7
frankietrimboli <62105951+frankietrimboli@users.noreply.github.com>
2022-02-14 11:52:05 -0500

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      url: 'https://www.thenetninja.co.uk',
+      showBooks: true,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assests/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assests/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assests/3.jpg', isFav: true },
+      ]
+    }
+  },
+  methods: {
+    toggleShowBooks() {
+        this.showBooks = !this.showBooks
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+      }
+    },
+    computed: {
+      filteredBooks() {
+        return this.books.filter((book) => book.isFav)
+    }
+  }
+)
+
+app.mount('#app')
